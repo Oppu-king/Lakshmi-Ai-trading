eac69d4d7f336037b36110f71f8160c997a444c6
Oppu-king <monjitkurmi96@gmail.com>
2025-07-29 04:07:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-rom flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import random, csv, os, requests
 from dotenv import load_dotenv
 from pathlib import Path
